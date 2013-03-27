b361b73e23732e1d96b040f7904dc47636238003
shuhuan <shuhuan2009@gmail.com>
2013-03-27 19:27:41 +0800

    shuhuan2009

---
+++ happinesstree-oauth2/happinesstree-oauth2-dao/src/test/java/com/happinesstree/oauth2/dao/mappertest/CommonDaoTest.java
@@ -0,0 +1,17 @@
+package com.happinesstree.oauth2.dao.mappertest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:test-env-context.xml" })
+public class CommonDaoTest {
+	
+	@Test
+	public void commonTest(){
+		
+	}
+
+}
---
+++ happinesstree-oauth2/happinesstree-oauth2-service/src/test/java/com/happinesstree/oauth2/service/test/BaseServiceTest.java
@@ -0,0 +1,17 @@
+package com.happinesstree.oauth2.service.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:test-env-context.xml" })
+public class BaseServiceTest {
+	
+	@Test
+	public void test() {
+
+	}
+
+}
